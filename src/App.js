74ae57e209b57a2c8416bc493e6dda8ed65ebae3
artofmayhem <chef_tony_miller@yahoo.com>
2021-04-26 10:38:29 -1000

    more commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar
+        color={"inherit"}
+        style={{ backgroundColor: "#222", color: "white" }}
+      >
+        <h1>Parallax-scss-template</h1>
+      </AppBar>
+      <div className={"wrapper"}>
+        {/* <div className={"snow layer1 a"}></div>
+        <div className={"snow layer1"}></div>
+        <div className={"snow layer2 a"}></div>
+        <div className={"snow layer2"}></div>
+        <div className={"snow layer3 a"}></div>
+        <div className={"snow layer3"}></div> */}
+        
+      </div>
+      <div className={"parallax-wrapper"}>
+        <div className={"content"}>
+          <h1>one Text</h1>
+        </div>
+      </div>
+      <div className={"regular-wrapper"}>
+        <div className={"content"}>
+          <h1>two Text</h1>
+        </div>
+      </div>
+      <div className={"parallax-wrapper2"}>
+        <div className={"content"}>
+          <h1>Three Text</h1>
+        </div>
+      </div>
+      <div className={"regular-wrapper2"}>
+        <div className={"content"}>
+          <h1>four Text</h1>
+        </div>
+      </div>
+      <div className={"parallax-wrapper3"}>
+        <div className={"content"}>
+          <h1>five Text</h1>
+        </div>
+      </div>
+      <div className={"regular-wrapper3"}>
+        <div className={"content"}>
+          <h1>six Text</h1>
+        </div>
+      </div>
+     
+    </>
   );
 }
 
